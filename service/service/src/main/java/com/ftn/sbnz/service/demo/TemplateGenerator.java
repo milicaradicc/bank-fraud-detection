765ff8b36af568b40f2fdb77b7de827c2af5e9b3
milicaradicc <milica.t.radic@gmail.com>
2026-06-28 14:59:37 +0200

    [ADD] more templates

--- service/service/src/main/java/com/ftn/sbnz/service/demo/TemplateGenerator.java
+++ service/service/src/main/java/com/ftn/sbnz/service/demo/TemplateGenerator.java
@@ -8,15 +8,29 @@ import java.io.InputStream;
 public class TemplateGenerator {
 
     public static void main(String[] args) throws Exception {
-        String basePath = "C:\\Users\\Lenovo\\Desktop\\projects\\bank-fraud-detection\\kjar\\kjar\\src\\main\\rule-templates\\rules\\templates\\";
-
-        InputStream template = new FileInputStream(basePath + "risk_thresholds.drt");
-        InputStream data = new FileInputStream(basePath + "risk_thresholds_data.xls");
+        String basePath = "C:\\Users\\Lenovo\\Desktop\\projects\\bank-fraud-detection\\kjar\\kjar\\src\\main\\resources\\templatetable\\";
 
         ExternalSpreadsheetCompiler converter = new ExternalSpreadsheetCompiler();
-        String drl = converter.compile(data, template, 3, 2);
 
-        System.out.println("=== GENERISANI DRL — kopiraj u kjar/src/main/resources/rules/templates/risk_thresholds_generated.drl ===");
-        System.out.println(drl);
+        // --- Template 1: Risk thresholds ---
+        InputStream t1 = new FileInputStream(basePath + "risk_thresholds.drt");
+        InputStream d1 = new FileInputStream(basePath + "risk_thresholds_data.xls");
+        String drl1 = converter.compile(d1, t1, 3, 2);
+        System.out.println("=== RISK THRESHOLDS DRL ===");
+        System.out.println(drl1);
+
+        // --- Template 2: MCC risk ---
+        InputStream t2 = new FileInputStream(basePath + "mcc_risk.drt");
+        InputStream d2 = new FileInputStream(basePath + "mcc_risk_data.xls");
+        String drl2 = converter.compile(d2, t2, 3, 2);
+        System.out.println("\n=== MCC RISK DRL ===");
+        System.out.println(drl2);
+
+        // --- Template 3: Country lists (crna/siva lista zemalja) ---
+        InputStream t3 = new FileInputStream(basePath + "country_lists.drt");
+        InputStream d3 = new FileInputStream(basePath + "country_lists_data.xls");
+        String drl3 = converter.compile(d3, t3, 3, 2);
+        System.out.println("\n\n=== COUNTRY LISTS DRL ===");
+        System.out.println(drl3);
     }
 }
